35e31116d48922b3ae551307f23faf89b3160dd2
Salma Shaik <salmashaik5820@gmail.com>
2026-02-24 15:39:58 +0530

    update player icon

--- script.js
+++ script.js
@@ -3,7 +3,24 @@ const audioPlayer = document.getElementById("audio-player");
 function playSong(file, title, artist) {
     audioPlayer.src = file;
     audioPlayer.play();
+    isPlaying = true;
 
     document.querySelector(".album-title").innerText = title;
     document.querySelector(".album-info").innerText = artist;
 }
+const playButton = document.querySelector(
+  ".player-control-icon[style*='2rem']"
+);
+
+let isPlaying = false;
+
+playButton.addEventListener("click", () => {
+    if (!audioPlayer.src) return;
+    if (isPlaying) {
+        audioPlayer.pause();
+        isPlaying = false;
+    } else {
+        audioPlayer.play();
+        isPlaying = true;
+    }
+});
