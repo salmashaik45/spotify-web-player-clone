0179029dded4702572a9234f0b6bc64f4ab08dad
Salma Shaik <salmashaik5820@gmail.com>
2026-02-24 21:07:46 +0530

    Add dynamic album cover update on song selection

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const albumCover = document.getElementById("album-cover");
 const audioPlayer = document.getElementById("audio-player");
 const playButton = document.getElementById("main-play-btn");
 const albumTitle = document.querySelector(".album-title");
@@ -6,13 +7,14 @@ const albumArtist = document.querySelector(".album-info");
 const playIcon = "./assets/player_icon3.png";
 const pauseIcon = "./assets/player_pause.png";
 
-function playSong(file, title, artist) {
+function playSong(file, title, artist, image) {
     audioPlayer.src = file;
     audioPlayer.play();
     isPlaying = true;
 
     albumTitle.innerText = title;
     albumArtist.innerText = artist;
+    albumCover.src = image;
 
     playButton.src = pauseIcon;
 }
