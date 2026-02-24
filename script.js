ab1fd4056eb1785230403d53a50e467d301f8f9b
Salma Shaik <salmashaik5820@gmail.com>
2026-02-24 15:49:03 +0530

    update script file

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
+// Get audio element
 const audioPlayer = document.getElementById("audio-player");
 
+// Get play button (make sure your HTML has id="main-play-btn")
+const playButton = document.getElementById("main-play-btn");
+
+// Get album title & artist
+const albumTitle = document.querySelector(".album-title");
+const albumArtist = document.querySelector(".album-info");
+
+// Track state
+let isPlaying = false;
+
+// Function to play selected song
 function playSong(file, title, artist) {
     audioPlayer.src = file;
     audioPlayer.play();
     isPlaying = true;
 
-    document.querySelector(".album-title").innerText = title;
-    document.querySelector(".album-info").innerText = artist;
+    albumTitle.innerText = title;
+    albumArtist.innerText = artist;
 }
-const playButton = document.querySelector(
-  ".player-control-icon[style*='2rem']"
-);
 
-let isPlaying = false;
-
+// Play / Pause button logic
 playButton.addEventListener("click", () => {
+
+    // If no song selected, do nothing
     if (!audioPlayer.src) return;
-    if (isPlaying) {
-        audioPlayer.pause();
-        isPlaying = false;
-    } else {
+
+    if (audioPlayer.paused) {
         audioPlayer.play();
         isPlaying = true;
+    } else {
+        audioPlayer.pause();
+        isPlaying = false;
     }
 });
